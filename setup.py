0aae58f1673851bc90e3da1bf80ea484fd3ebb9e
gkpty <gkardonski@gmail.com>
2022-02-03 13:57:36 -0500

    add requirements

--- setup.py
+++ setup.py
@@ -6,5 +6,6 @@ setup(
   description='SDK for the panadata API',
   author='Gabriel Kardonski',
   author_email='gabriel@panadata.net',
-  packages=['panadata-api-sdk'],  #same as name
+  packages=['panadata-api-sdk'],
+  install_requires=['requests', 'unidecode'],
 )
